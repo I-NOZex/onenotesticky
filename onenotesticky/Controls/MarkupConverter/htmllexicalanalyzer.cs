d51b144107e2a54a0a5112f5807c0264d9bbeec7
I-NOZex <simplesmente_moi@hotmail.com>
2015-08-21 12:36:45 +0100

    Deleted some unused files

--- onenotesticky/Controls/MarkupConverter/htmllexicalanalyzer.cs
+++ onenotesticky/Controls/MarkupConverter/htmllexicalanalyzer.cs
@@ -21,7 +21,7 @@ namespace MarkupConverter
     /// recognizes tokens as groups of characters separated by arbitrary amounts of whitespace
     /// also classifies tokens according to type
     /// </summary>
-    internal class HtmlLexicalAnalyzer : IDisposable
+    internal class HtmlLexicalAnalyzer
     {
         // ---------------------------------------------------------------------
         //
